54f031adcedc52157fae3f82cd12e49ee7e90268
Hasitha Dananjaya <140165356+Hasitha12389@users.noreply.github.com>
2024-07-12 16:25:49 +0530

    Update q1.scala

--- q11.scala
+++ q11.scala
@@ -1,46 +1,44 @@
-object q11 {
-  val itemNames = Array("biscuits", "sugar", "cake", "rice", "soap")
-  val itemQuantities = Array(20, 10, 5, 20, 15)
+object q1{
+  // Inventory means Array of items in the stock
 
-  def displayInventory(): Unit = {
-    println("-----------------Inventory--------------------------")
-    itemNames.zip(itemQuantities).foreach { // zip method pair each element of the two arrays
-      case (name, quantity) =>              //for each print each pair 
-      println(s"$name -->> $quantity")                                  
+  //q1 part 1
+  def displayInventory(arr1: Array[String], arr2: Array[Int]): Unit = {
+    arr1.zip(arr2).foreach { case (str, num) =>  // zip - pair the coresponding elements of both String and Int Arrays
+      println(s"$str -->> $num")    // for each loop print that all pairs in this format
     }
   }
 
-  def restockItem(name: String, quantity: Int): Unit = {
-    val index = itemNames.indexOf(name)         // find out index of given name in itemNames array and store it in val.
-    if (index != -1) {
-      itemQuantities(index) += quantity
-      println(s"Restocked $name. Now ${itemQuantities(index)} $name are available.")
-    } else {
-      println(s"$name is does not exist in inventory.")
-    }
+
+  //q2 part 2
+  def createInventory(arr1: Array[String], arr2: Array[Int]): Map[String, Int] = {
+    arr1.zip(arr2).toMap   // tomap - method convert pairs into map (key-value pairs (name - quantities)) 
   }
 
-  def sellItem(name: String, quantity: Int): Unit = {
-    val index = itemNames.indexOf(name)
-    if (index != -1) {
-      if (itemQuantities(index) >= quantity) {
-        itemQuantities(index) -= quantity
-        println(s"Sold $name. Remaining $name quantity is ${itemQuantities(index)}.")
-      } else {
-        println(s"Not enough quantity of $name to sell. Only available ${itemQuantities(index)}.")
-      }
-    } else {
-      println(s"$name is does not exist in inventory.")
+  def restockItem(name: String, quantity: Int, inventory: Map[String, Int]): Map[String, Int] = {        // Map[String, Int] String int pairs 
+    inventory.get(name) match {           // get the given argument name and check it is exists in the inventory
+      case Some(existingQuantity) =>
+        val updatedQuantity = existingQuantity + quantity
+        println("Restocked item")
+        inventory.updated(name, updatedQuantity)
+
+      case None =>
+        println("Item does not exist in inventory.")
+        inventory
     }
+    //This function return the updated inventory map
   }
 
   def main(args: Array[String]): Unit = {
-    displayInventory()
-    restockItem("biscuits", 17)
-    restockItem("banana",10)
-    sellItem("sugar", 8)
-    sellItem("cake",7)
-    sellItem("jam",4)
-    displayInventory()
+    val names = Array("Biscuits", "Sugar", "Cake", "Rice", "Soap")
+    val quantities = Array(20, 10, 5, 20, 15)
+
+    displayInventory(names, quantities)
+
+    val initialInventory = createInventory(names, quantities)                 // inventory map
+    var updatedInventory = restockItem("Biscuits", 17, initialInventory)
+    updatedInventory = restockItem("toffy", 5, initialInventory)
+    // You can continue using 'updatedInventory' in your program
+
+    println(updatedInventory)
   }
 }
